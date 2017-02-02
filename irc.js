31b3513cd447e39a46aefd4f574dac68b6198c4f
Tracks <anon.dark97@gmail.com>
2017-02-02 22:16:01 +0100

    Update irc.js

--- irc.js
+++ irc.js
@@ -4,6 +4,26 @@
 *                    *
 *********************/
 
+// Récupération des donnée par protocol XML
+if(window.XMLHttpRequest) { obj = new XMLHttpRequest(); } // Chrome, IE, etc...
+else if(window.ActiveXObject) { obj = new ActiveXObject("Microsoft.XMLHTTP"); } // Pour FireFox
+if(obj.overrideMimeType) { obj.overrideMimeType("text/xml"); } // Safari le sheitan navigateur d'apple
+
+function refresh_tchat(tchat) { // Fonction de récupération de donnée d'un fichier
+	var output_tchat = document.getElementById('tchat_area');
+	
+	obj.open('GET', tchat, false);
+	obj.send(null);
+	
+	output_tchat.innerHTML = obj.responseText;
+}
+
+function startTime() {
+	var output_tchat = document.getElementById('tchat_area');
+	t = setTimeout(function() { startTime(); }, 500);
+	refresh_tchat(lien);
+}
+
 function info_tchat(msg) {
 	var entry = document.getElementById('text_input');
 	
