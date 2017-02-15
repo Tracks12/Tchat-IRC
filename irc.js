40a1790bf3373e0bd3a50e490654992015501f54
Tracks <anon.dark97@gmail.com>
2017-02-15 02:32:18 +0100

    Update irc.js

--- irc.js
+++ irc.js
@@ -29,6 +29,17 @@ function startTime() {
 	refresh_tchat(lien);
 }
 
+function info_tchat(msg) {
+ 	var entry = document.getElementById('text_input');
+ 	
+ 	if(msg == 'Ecrivez votre message...') {
+ 		entry.value = '';
+ 	}
+ 	else if(msg == '') {
+ 		entry.value = 'Ecrivez votre message...';
+ 	}
+ }
+
 /******
 * END *
 ******/
